624de8136a78f223292b513d59ee405ba96a540a
Rodrigo Orellana <rodrigo.orelana@gmail.com>
2021-04-29 20:10:44 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react'
+import PokemonName from './components/PokemonName'
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [poke, setPoke] = useState([])
+    const [counter, setCounter] = useState(0)
+
+    const llamarApi = async () => {
+        try {
+            if (counter < 990) {
+                let counterplus = counter + 20
+                let resultado = await axios.get(
+                    'https://pokeapi.co/api/v2/pokemon?offset=' +
+                        counterplus +
+                        '&limit=' +
+                        20,
+                )
+                const { results } = resultado.data
+                const names = results.map((x) => x.name)
+                setPoke(names)
+                setCounter(counter + 20)
+            } else setCounter(0)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    return (
+        <div className="App">
+            <h1>Pokemon API</h1>
+            <button onClick={llamarApi}>Presione aqui</button>
+            <PokemonName poke={poke} />
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/PokemonName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PokemonName = (props) => {
+    const pokem = props.poke
+
+    const pokemons = pokem.map((pName, index) => <div key={index}>{pName}</div>)
+
+    return <>{pokemons}</>
+}
+
+export default PokemonName
